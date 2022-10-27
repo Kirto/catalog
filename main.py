e34f7b141ecd84ff073ff8282eb3116268822a02
Kirto <Cobalt121Kira@gmail.com>
2022-10-27 18:17:27 +0600

    [upd] Added new methods in class GUIForDatabase:           - config_gui           - start_app
          Refactoring code in class GUIForDatabase.
          Refactoring code in condition __name__

--- main.py
+++ main.py
@@ -73,28 +73,80 @@ class WorkWithDatabase:
         return data
 
 
-class GUIForDatabase:
-    def __int__(self):
-        self.gui = tk.Tk()
+class GUIForDatabase(tk.Tk):
+    def __int__(self, title_gui: str = 'Tk', width: int = 400, height: int = 400,
+                offsetx: int = 100, offsety: int = 100, png_ico_filepath: str = '',
+                maxsizex: int = 500, maxsizey: int = 500, minsizex: int = 100, minsizey: int = 100,
+                resizablexy: bool = True, bg_gui: str = 'gray'):
+        """
 
-    def open_gui(self):
+        :param1 title_gui: name of application
+        :param2 width: width of application
+        :param3 height: height of application
+        :param4 offsetx: to offset the left top application angle for 'x'
+        :param5 offsety: to offset the left top application angle for 'y'
+        :param6 png_ico_filepath: path to png file for make ico for application
+        :param7 maxsizex: maximum size of application for 'x'
+        :param8 maxsizey: maximum size of application for 'y'
+        :param9 minsizex: minimum size of application for 'x'
+        :param10 minsizey: minimum size of application for 'x'
+        :param11 resizablexy: resizable for horizontal and vertical of application
+        :param12 bg_gui: background of application
+        :return: None
+        """
+        super()
+        self.title(title_gui)
+        self.width = width
+        self.height = height
+        self.offsetx = offsetx
+        self.offsety = offsety
+        self.ico_filepath = png_ico_filepath
+        self.maxsizex = maxsizex
+        self.maxsizey = maxsizey
+        self.minsizex = minsizex
+        self.minsizey = minsizey
+        self.resizablex = resizablexy
+        self.resizabley = resizablexy
+        self.bg = bg_gui
+
+    def config_gui(self):
+        self.geometry(f"{self.width}x{self.height}+{self.offsetx}+{self.offsety}")
+        self.maxsize(self.maxsizex, self.maxsizey)
+        self.minsize(self.minsizex, self.minsizey)
+        self.resizable(self.resizablex, self.resizabley)
+        if self.ico_filepath:
+            self.iconphoto(False, tk.PhotoImage(file=self.ico_filepath))
+        self.config(bg=self.bg)
+
+    def app_create_widgets(self):
         pass
 
-    def gui_mainloop(self):
-        self.gui.mainloop()
+    def start_app(self):
+        """
+
+        This function for start application:
+            - init
+            - configure
+            - create widgets
+        :return: None
+        """
+        self.__int__()
+        self.config_gui()
+        self.app_create_widgets()
+        self.mainloop()
 
 
 if __name__ == '__main__':
-    table = 'items'
-    name = '6ES'
-    ozm = 707801
-    new_val = []
+    # table = 'items'
+    # name = '6ES'
+    # ozm = 707801
+    # new_val = []
 
-    data = WorkWithDatabase()
-    data.update_data_in_db(table, new_value=new_val, field='')
+    # data = WorkWithDatabase()
+    # data.update_data_in_db(table, new_value=new_val, field='')
 
     app = GUIForDatabase()
-    app.open_gui()
+    app.start_app()
 
     # print(load_one_item_by_name_from_db(table, name))
 
