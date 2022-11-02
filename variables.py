6df879827877112b9f16fd8b08015f59e5a04624
Z-PC/Work <Z-PC@work.inc>
2022-11-02 12:51:30 +0600

    [upd] Refactor code, fix bugs

--- variables.py
+++ variables.py
@@ -4,22 +4,24 @@ GPPORT = '5432' # your_database_port
 PGDATABASE = 'catalog_db' # your_database_name
 PGUSER = 'postgres' # your_database_username
 PGPASSWORD = '1' # your_database_secret_password
+PGTABLE = 'Items'
+PGSCHEMA = 'public'
 
 
 # GUI variables
-TITLE = 'Catalog'
+NAME_GUI = 'Catalog'
 WIDTH = 500
 HEIGHT = 500
-MAXSIZEX = WIDTH * 2
-MAXSIZEY = HEIGHT * 2
-MINSIZEX = WIDTH / 2
-MINSIZEY = HEIGHT / 2
-OFFSETX = 0
-OFFSETY = 0
+MAXSIZEX = round(WIDTH * 1.5)
+MAXSIZEY = round(HEIGHT * 1.5)
+MINSIZEX = WIDTH // 2
+MINSIZEY = HEIGHT // 2
+OFFSETX = 100
+OFFSETY = 100
 BGCOLOR = 'gray'
 FGCOLOR = 'white'
 BTNBGCOLOR = 'black'
 BTNFGCOLOR = 'white'
 RESIZE = True
-PNG_ICO_PATH = 'app_ico.png'  # path to file *.png for creating ico application
+PNG_ICO_PATH = ''  # path to file *.png for creating ico application
 
