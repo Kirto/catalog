235e60d41809229d126050372c59ad675a229e70
Kirto <Cobalt121Kira@gmail.com>
2022-12-16 15:36:37 +0600

    [upd] Refactoring code in .gitignore and requaroments.txt [upd] Added pc_view_Qt5_UI.py in folder views

---
+++ views/pc_view_Qt5_UI.py
@@ -0,0 +1,53 @@
+"""
+
+This block created GUI for view information from postgres database.
+
+Author: Kirto
+version: zero
+"""
+
+from PyQt5 import QtWidgets
+import sys
+
+
+class Window(QtWidgets.QWidget):
+
+	def __int__(self):
+		super(Window, self).__init__()
+
+		dx, dy, w, h, title = 300, 250, 500, 500, 'catalog'
+
+		self.setWindowTitle(title)
+		self.setGeometry(dx, dy, w, h)
+
+		self.main_text = QtWidgets.QLabel(self)
+		self.main_text.setText('This is base label!')
+		self.main_text.move(100, 240)  # move in window (relative coordinate)
+		self.main_text.adjustSize()
+
+		# self.second_text = QtWidgets.QLabel(self)
+
+		self.button = QtWidgets.QPushButton(self)
+		self.button.move(70, 150)
+		self.button.setText('Push me~!')
+		self.button.setFixedWidth(200)
+		self.button.clicked.connect(self.clicked_button)
+
+	def clicked_button(self):
+		self.second_text.setText('New label')
+		self.second_text.move(100, 10)
+
+
+def create_app():
+	app = QtWidgets.QApplication(sys.argv)
+	print('asdasd')
+
+	name = Window()
+
+	name.show()
+	# print(Window.mro())  # rules entry
+	sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+	create_app()
